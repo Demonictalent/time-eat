d11b41b2d5dbe7c37abc904467c53c27fd753a02
talent <talent>
2019-03-14 14:46:08 +0800

    add layouts folder to write global scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-
 import RouterController from './routers/RouterController';
-import './App.scss';
+import './layouts/App.scss';
 
 class App extends Component {
   render() {
