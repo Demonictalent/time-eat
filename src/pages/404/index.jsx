0e28d21d22e9ae61103cea1e1affa7d99bb2a468
talent <talent>
2019-03-14 15:03:14 +0800

    404 page fixing

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import './NotFound.scss'
 
 class NotFound extends Component {
@@ -16,7 +17,8 @@ class NotFound extends Component {
         <div className="not-found-container">
           <div className="main">
               <h1>404</h1>
-              <h1>{this.state.user}</h1>
+              <h1>仿佛走错地方了...</h1>
+              <Link to ='/'><h3>回到主页！</h3></Link>
           </div>
         </div>
       </div>
