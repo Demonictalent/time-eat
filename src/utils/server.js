aca807268eacf34952261698c1f60349117df31b
talent <talent>
2019-03-27 16:53:16 +0800

    add axios module

---
+++ src/utils/server.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import qs from 'qs';
+
+axios.interceptors.request.use(
+  config => {
+    config.baseUrl = '/api/'
+    config.withCredentials = true
+    config.timeout = 6000
+    let token = sessionStorage.getItem('access_token')
+    let csrf = store.getters.csrf
+    if (token) {
+      config.headers = {
+        'access-token': token,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }
+  }
+)
+
+
+export function login (data) {
+  return axios.post('', data)
+}
+
+export function register (data) {
+  return axios.get('', data)
+}
